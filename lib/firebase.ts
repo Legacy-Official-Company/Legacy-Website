63b4690481510f3ff56a3bbc1b12b3641aa48ff7
Legacy-Official-Company <legacy.official.corporation@gmail.com>
2025-10-05 16:59:36 +0300

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -6,14 +6,15 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAxsAZYxELSg0dwUvsON820fOaRmJi6zas",
-  authDomain: "selling-website-digital.firebaseapp.com",
-  projectId: "selling-website-digital",
-  storageBucket: "selling-website-digital.firebasestorage.app",
-  messagingSenderId: "857075190348",
-  appId: "1:857075190348:web:c703382c845c94378c82e9",
-  measurementId: "G-2E4HG0Q9RD"
+  apiKey: "AIzaSyBRGI__MvK_cqsR89_r3Qp3mb6LshRun3c",
+  authDomain: "legacy-3cbf3.firebaseapp.com",
+  projectId: "legacy-3cbf3",
+  storageBucket: "legacy-3cbf3.firebasestorage.app",
+  messagingSenderId: "464872189077",
+  appId: "1:464872189077:web:b4827cb0815cbe87e4bf9c",
+  measurementId: "G-KQJ5W3W2QK"
 };
 
 // Initialize Firebase with error handling
